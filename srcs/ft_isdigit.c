34b38b3150a719cdade4fe94ef32273203ef125d
amiguez <amiguez@student.42lyon.fr>
2022-06-04 17:16:13 +0200

    small tweak ft_isdigit

--- srcs/ft_isdigit.c
+++ srcs/ft_isdigit.c
@@ -6,12 +6,18 @@
 /*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 13:39:05 by amiguez           #+#    #+#             */
-/*   Updated: 2022/05/29 07:03:59 by amiguez          ###   ########.fr       */
+/*   Updated: 2022/06/04 17:14:59 by amiguez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
+/**
+ * @brief return 1 if c is a digit, 0 otherwise
+ * 
+ * @param c 
+ * @return int 
+ */
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
