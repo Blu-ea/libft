a2f5b39eaf569f5956eaf34c50ccb87cea496ccb
Blue-bg <82770859+Blue-bg@users.noreply.github.com>
2022-10-06 05:03:46 +0200

    Update ft_strncmp.c
    Crash if one of the string where NULL pointer

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -29,8 +29,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
+	if (!str1 && !str2)
+		return (0);
+	if (!str1)
+		return (-*str2);
+	if (!str2)
+		return (*str1);
 	i = 0;
-	while (n > i && str1[i] != '\0' && str2[i] != '\0')
+	while (n > i && str1[i] != 0 && str2[i] != 0)
 	{
 		if (s1[i] != s2[i])
 			return (str1[i] - str2[i]);
